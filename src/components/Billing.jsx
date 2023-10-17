48e0839704ffd932532030dd3962d5c50676b577
devopsdbroe <dbroe1995@gmail.com>
2023-10-17 11:28:19 -0400

    Fixed ID in Billing

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -2,7 +2,7 @@ import { apple, bill, google } from "../assets";
 import styles, { layout } from "../style";
 
 const Billing = () => (
-	<section id="section" className={layout.sectionReverse}>
+	<section id="product" className={layout.sectionReverse}>
 		<div className={layout.sectionImgReverse}>
 			<img
 				src={bill}
